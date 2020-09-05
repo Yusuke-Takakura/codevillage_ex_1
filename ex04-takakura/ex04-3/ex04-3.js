3f68fcd9a8ff64cfaa2bcf05e80460925032d941
Yusuke-Takakura <p8g43blf@s.okayama-u.ac.jp>
2020-09-05 18:06:41 +0900

    practice添削

--- ex04-takakura/ex04-3/ex04-3.js
+++ ex04-takakura/ex04-3/ex04-3.js
@@ -2,15 +2,25 @@ const scores = [13, 67, 18, 54, 30, 22, 89, 49, 21, 65]
 
 function scoring(score) {
     if (score >= 90) {
-        console.log('秀')
+        const eva = '秀'
+        return eva
+        // console.log('秀')
     } else if (score >= 75) {
-        console.log('優')
+        const eva = '悠'
+        return eva
+        // console.log('優')
     } else if (score >= 60) {
-        console.log('良')
+        const eva = '中'
+        return eva
+        // console.log('良')
     } else if (score >= 30) {
-        console.log('可')
+        const eva = 't'
+        return eva
+        // console.log('可')
     } else {
-        console.log('不可')
+        const eva = 'h'
+        return eva
+        // console.log('不可')
     }
 }
 
--- practice/practice.js
+++ practice/practice.js
@@ -8,16 +8,14 @@ const dragon = [
 // console.log(power);
 
 function measure(chara) {
-    let strong = [];
     for (let i = 0; i < 3; i++) {
-        strong[0] = chara[i].power;
-        if (strong[0] >= 40000) {
+        if (chara[i].power >= 40000) {
             const eva = '最強';
             console.log(eva);
-        } else if (strong[0] >= 30000) {
+        } else if (chara[i].power >= 30000) {
             const eva = '中堅';
             console.log(eva);
-        } else if (strong[0] >= 25000) {
+        } else if (chara[i].power >= 25000) {
             const eva = '最弱';
             console.log(eva);
         }
