cf390c0fb34c40f83f1d0da729e71521361f18d8
Yusuke-Takakura <p8g43blf@s.okayama-u.ac.jp>
2020-09-18 23:47:39 +0900

    課題６修正

---
+++ ex06-takakura/ex06-1.js
@@ -0,0 +1,16 @@
+const items = [
+    // 日  月  火  水  木  金  土
+    [14, 93, 89, 51, 85, 59, 33],
+    [69, 27, 40, 76, 25, 21, 55],
+    [55, 60, 3, 28, 49, 5, 91],
+    [19, 56, 92, 66, 53, 80, 13],
+]
+
+const sums = items.map((item) => {
+    let sum = 0;
+    item.forEach(function (elements) {
+        sum += elements;
+    })
+    return sum;
+})
+console.log(sums);
